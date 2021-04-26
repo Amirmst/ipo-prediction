8baf74eff95d8bfafdcee2aef8ad6e57197e8965
Amir Mostafavi <amireza.mostafavi@yahoo.com>
2021-04-26 12:03:13 -0500

    Used the ready fpr preprocess file to get descriptions

--- scrapers/data_scraper.py
+++ scrapers/data_scraper.py
@@ -6,14 +6,14 @@ stock_symbols = []
 API_ENDPOINT = "https://financialmodelingprep.com/api/v3/profile/{fsymbol}?apikey=1df59303d164b13ecff41fe2c9af6ef8"
 
 missing = 0
-with open('IPODataFull.csv', mode='r', encoding='latin1') as source_file:
+with open('../ready_for_preprocessing.csv', mode='r', encoding='latin1') as source_file:
     csvreader = csv.reader(source_file)
-    #fields = next(csvreader)
+    fields = next(csvreader)
 
-    with open('data-1.csv', mode='a', encoding='utf-8') as dest_file:
+    with open('../ready_for_preprocessing_description.csv', mode='w', encoding='utf-8') as dest_file:
         csvwriter = csv.writer(dest_file)
-        #fields.append('Description')
-        #csvwriter.writerow(fields) 
+        fields.append('Description')
+        csvwriter.writerow(fields) 
         for row in csvreader:
             response = requests.get(API_ENDPOINT.format(fsymbol=row[0]))
             response.raise_for_status()
@@ -30,4 +30,4 @@ with open('IPODataFull.csv', mode='r', encoding='latin1') as source_file:
             row.append(description)
             csvwriter.writerow(row)
 
-print("MISSING DATA:", missing)
+print("****** MISSING DATA ********:", missing)
